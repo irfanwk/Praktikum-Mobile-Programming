b7a45be7b90fece71a5bd6b99c9d515bf2bafb5e
irfanwk <irfan.wk.2705@gmail.com>
2026-04-11 12:18:35 +0700

    P6 SK1 first commit

---
+++ P6SK1PemutarMusikSederhana/app/src/main/java/com/muhammadirfanwirakusuma/p6sk1pemutarmusiksederhana/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.muhammadirfanwirakusuma.p6sk1pemutarmusiksederhana
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.muhammadirfanwirakusuma.p6sk1pemutarmusiksederhana.ui.theme.P6SK1PemutarMusikSederhanaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            P6SK1PemutarMusikSederhanaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    P6SK1PemutarMusikSederhanaTheme {
+        Greeting("Android")
+    }
+}
